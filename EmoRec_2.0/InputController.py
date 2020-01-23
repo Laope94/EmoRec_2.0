251f150d164c0215d93b6d4b044dbb2abcc06948
Laope94 <timotej.sulka@gmail.com>
2020-01-23 20:01:19 +0100

    added input controller and helper functions loading active input devices into dropdown list

---
+++ EmoRec_2.0/InputController.py
@@ -0,0 +1,24 @@
+class InputController(object):
+    import sounddevice as sd
+   
+    #returns dictionary of devices and their IDs
+    def getDeviceList(self):
+        deviceQueryResult = self.sd.query_devices()
+        hostapiQueryResult = self.sd.query_hostapis()
+        deviceList = dict()
+        for i in range(len(deviceQueryResult)):
+            if(deviceQueryResult[i]['max_input_channels']>0):
+                #deviceList.append({"id" : i,
+                #                   "name" : deviceQueryResult[i]["name"],
+                #                   "hostapi" : hostapiQueryResult[deviceQueryResult[i]["hostapi"]]["name"],
+                #                   "in" : deviceQueryResult[i]["max_input_channels"],
+                #                   "out" : deviceQueryResult[i]["max_output_channels"],
+                #                   "default_samplerate" : deviceQueryResult[i]["default_samplerate"]})
+                deviceList["" + deviceQueryResult[i]["name"] + 
+                           ", " + hostapiQueryResult[deviceQueryResult[i]["hostapi"]]["name"] + 
+                           ", (in: " + str(deviceQueryResult[i]["max_input_channels"]) +
+                           ", out: " + str(deviceQueryResult[i]["max_output_channels"]) + ")" +
+                           ", " + str(deviceQueryResult[i]["default_samplerate"]) + "Khz"] = i
+        return deviceList
+
+
